0b197096f601be26636fd6acd85fab6c989e0a6b
rajon-git <rajon.zhsust15@gmail.com>
2024-02-24 22:47:02 +0600

    password reset token create

--- model/userModel.js
+++ model/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
@@ -42,6 +43,9 @@ var userSchema = new mongoose.Schema({
     refreshToken: {
         type: String,
     },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date
 }
 , {
     timestamps: true
@@ -49,6 +53,10 @@ var userSchema = new mongoose.Schema({
 
 userSchema.pre('save', async function(next)
 {
+    if(!this.isModified("password"))
+    {
+        next();
+    }
     const salt = await bcrypt.genSaltSync(10);
     this.password = await bcrypt.hash(this.password,salt);
 
@@ -56,5 +64,15 @@ userSchema.pre('save', async function(next)
 
 userSchema.methods.isPasswordMatched = async function(enteredPassword){
     return await bcrypt.compare(enteredPassword,this.password);
+}
+
+userSchema.methods.createPasswordResetToken = async function(){
+    const resetToken = crypto.randomBytes(32).toString("hex");
+    this.passwordResetToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+    this.passwordResetExpires= Date.now()+30*60*1000;
+    return resetToken;
 }
 module.exports = mongoose.model('User', userSchema);
