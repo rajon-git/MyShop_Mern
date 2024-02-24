367c7d00d71bb2e85fab93ef073e29b3140e7bd5
rajon-git <rajon.zhsust15@gmail.com>
2024-02-24 22:35:29 +0600

    filtering,limitations,fields,sorting api added

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -30,8 +30,56 @@ const getaproduct = asyncHandler(async(req,res)=>{
 //get all produxt
 
 const getallProduct = asyncHandler(async(req,res)=>{
+  console.log(req.query);
     try {
-        const getallProduct = await Product.find();
+
+      //filtering
+        const queryObj = {...req.query};
+        const excludeFields =["page", "sort","limit","fields"];
+        excludeFields.forEach(el => delete queryObj[el]);
+
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) =>`$${match}`);
+        
+
+        let query = Product.find(JSON.parse(queryStr));
+
+        //sorting
+        if(req.query.sort){
+          const sortBy = req.query.sort.split(',').join(" ");
+          query=query.sort(sortBy);
+        }
+        else
+        {
+          query = query.sort("-createdAt");
+        }
+
+        //limiting the fields
+
+        if(req.query.fields)
+        {
+          const fields = req.query.fields.split(",").join(" ");
+          query = query.select(fields);
+
+        }
+        else
+        {
+          query = query.select('-__v');
+        }
+
+        //pagination
+
+        const page = req.query.page;
+        const limit = req.query.limit;
+        const skip = (page-1)*limit;
+        query = query.skip(skip).limit(limit);
+        if(req.query.page)
+        {
+          const productCount = await Product.countDocuments();
+          if(skip >= productCount) throw new Error("This page doesn't exists");
+        }
+        const product = await query;
+        res.json(product);
         res.json(getallProduct);
     } catch (error) {
         throw new Error(error);
