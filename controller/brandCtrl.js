51e34529936b44f28742fc8757f7e5f1d91777c2
rajon-git <rajon.zhsust15@gmail.com>
2024-02-26 19:02:44 +0600

    all brand get api create

--- controller/brandCtrl.js
+++ controller/brandCtrl.js
@@ -46,6 +46,15 @@ const createBrand = asyncHandler(async (req, res) => {
     }
   });
 
+  const getallBrand = asyncHandler(async (req, res) => {
+    try {
+      const getallBrand = await Brand.find();
+      res.json(getallBrand);
+    } catch (error) {
+      throw new Error(error);
+    }
+  });
+
 module.exports = {
     createBrand,
     updateBrand,
