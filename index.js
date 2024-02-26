9e139a97ed4db65ec9179b4adab77a4b4f94da68
rajon-git <rajon.zhsust15@gmail.com>
2024-02-26 18:18:51 +0600

    category route complete

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
 const blogRouter = require("./routes/blogRoute");
+const categoryRouter = require("./routes/categoryRoute");
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const morgan = require("morgan");
 
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use('/api/user', authRouter);
 app.use('/api/product', productRouter);
 app.use('/api/blog', blogRouter);
+app.use('/api/category', categoryRouter);
 
 app.use(notFound);
 app.use(errorHandler);
---
+++ routes/categoryRoute.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { createCategory } = require("../controller/categoryCtrl");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
+const router = express.Router();
+
+router.post("/", authMiddleware, isAdmin, createCategory);
+
+module.exports = router;
