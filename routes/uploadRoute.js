16d79f6c43a06806af8deedbd2ad5bb887be64e1
rajon-git <rajon.zhsust15@gmail.com>
2024-03-21 15:56:59 +0600

    bug fixed

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -4,7 +4,7 @@ const { isAdmin,authMiddleware } = require("../middlewares/authMiddleware");
 const { uploadPhoto, productImgResize } = require("../middlewares/uploadImages");
 const router = express.Router();
 
-router.post("/upload/",authMiddleware,isAdmin, uploadPhoto.array('images', 10), productImgResize, uploadImges);
+router.post("/",authMiddleware,isAdmin, uploadPhoto.array('images', 10), productImgResize, uploadImges);
 router.delete("/delete-img/:id",authMiddleware,isAdmin, deleteImges);
 
 module.exports = router;
