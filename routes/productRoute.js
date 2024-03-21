c884b5ac3973b318aae160d73f7a4743743f44ac
rajon-git <rajon.zhsust15@gmail.com>
2024-03-21 15:19:44 +0600

    bug fixed

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { createProduct, getaproduct,getallProduct, updateProduct, deleteProduct, addToWishlist,rating } = require("../controller/productCtrl");
+const { createProduct, getaproduct,getallProduct,deleteImges, updateProduct, deleteProduct, addToWishlist,rating, uploadImges } = require("../controller/productCtrl");
 const { isAdmin,authMiddleware } = require("../middlewares/authMiddleware");
+const { uploadPhoto, productImgResize } = require("../middlewares/uploadImages");
 const router = express.Router();
 
 router.post("/",authMiddleware,isAdmin, createProduct);
-
+router.post("/upload/",authMiddleware,isAdmin, uploadPhoto.array('images', 10), productImgResize, uploadImges);
 
 router.get("/:id", getaproduct);
 router.put("/wishlist",authMiddleware, addToWishlist);
@@ -13,5 +14,6 @@ router.put("/rating",authMiddleware, rating);
 router.get("/", getallProduct);
 router.put("/:id",authMiddleware,isAdmin, updateProduct);
 router.delete("/:id",authMiddleware,isAdmin, deleteProduct);
+router.delete("/delete-img/:id",authMiddleware,isAdmin, deleteImges);
 
 module.exports = router;
