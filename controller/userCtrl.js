2690cb38906632e117d36dc2db365bb5ebb848b9
rajon-git <rajon.zhsust15@gmail.com>
2024-04-29 22:53:54 +0600

    update forgot password ctrl

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -276,7 +276,7 @@ const forgotPasswordToken = asyncHandler(async (req, res) => {
   try {
       const token = await user.createPasswordResetToken();
       await user.save();
-      const resetURL = `Hi, Please follow this link to reset Your Password. This link is valid till 10 minutes from now. <a href='http://localhost:5000/api/user/reset-password/${token}'>Click Here</a>`;
+      const resetURL = `Hi, Please follow this link to reset Your Password. This link is valid till 10 minutes from now. <a href='http://localhost:3000/reset-password/${token}'>Click Here</a>`;
       const data = {
           to: email,
           text: "Hey User",
