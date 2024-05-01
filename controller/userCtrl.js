b190163104ab198b1528c0ba9e34b516a7d1d58e
rajon-git <rajon.zhsust15@gmail.com>
2024-05-01 23:45:38 +0600

    get yearly orders ctrl implemented

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -480,6 +480,36 @@ const getMonthWiseOrderCount = asyncHandler(async(req,res)=>{
   res.json(data);
 })
 
+const getYearlyTotalIncome = asyncHandler(async(req,res)=>{
+  let monthNames= ["January","February","March","April","May","June","July",
+            "August","September","October","November","December"];
+  let d = new Date();
+  let endDate = "";
+  d.setDate(1);
+  for (let index = 0; index < 11; index++) {
+    d.setMonth(d.getMonth()-1);
+    endDate = monthNames[d.getMonth()]+ " " + d.getFullYear()
+    
+  }
+  const data = await Order.aggregate([
+    {
+      $match : {
+        createdAt: {
+          $lte:new Date(),
+          $gte: new Date(endDate)
+        }
+      }
+    },
+    {
+      $group: {
+        _id:null,
+        amount:{ $sum: "$_id" }
+      }
+    }
+  ])
+  res.json(data);
+})
+
 // const emptyCart = asyncHandler(async (req, res) => {
 //   const { _id } = req.user;
 //   validateMongoDbId(_id);
@@ -646,5 +676,6 @@ module.exports = {
                  removeProductFromCart,
                  updateQuantityFromCart,
                  getMonthWiseOrderIncome,
-                 getMonthWiseOrderCount
+                 getMonthWiseOrderCount,
+                 getYearlyTotalIncome
                 };
