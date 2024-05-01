27451f2c3dbf344c23ca182dd1d7d0d2358625f9
rajon-git <rajon.zhsust15@gmail.com>
2024-05-01 23:21:29 +0600

    updste on middleware and also update order model

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,28 +2,62 @@ const User = require("../model/userModel");
 const jwt =  require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 
-const authMiddleware = asyncHandler(async(req,res,next)=>{
-    let token;
-    if(req?.headers?.authorization?.startsWith("Bearer"))
-    {
-        token = req.headers.authorization.split(" ")[1];
-        try {
-            if(token)
-            {
+// const authMiddleware = asyncHandler(async(req,res,next)=>{
+//     let token;
+//     if(req?.headers?.authorization && req?.headers?.authorization?.startsWith("Bearer"))
+//     {
+//         token = req?.headers?.authorization.split(" ")[1];
+//         try {
+//             if(token)
+//             {
+//                 const decode = jwt.verify(token, process.env.JWT_SECRET);
+//                 const user = await User.findById(decode?._id);
+//                 if (user) {
+//                     req.user = user;
+//                     next();
+//                 } 
+//                 else {
+//                     throw new Error("User not found");
+//                 }
+//             }
+//             else {
+//                 throw new Error("Authorization header missing or invalid");
+//             }
+//         } catch (error) {
+//             throw new Error("No authorized token, Please login again")
+//         }
+//     }
+//     else
+//     {
+//         throw new Error("There is no token with header");
+//     }
+// });
+
+const authMiddleware = asyncHandler(async (req, res, next) => {
+    try {
+        let token;
+        if (req?.headers?.authorization && req?.headers?.authorization?.startsWith("Bearer")) {
+            token = req?.headers?.authorization?.split(" ")[1];
+            if (token) {
                 const decode = jwt.verify(token, process.env.JWT_SECRET);
-                const user = await User.findById(decode?.id);
-                req.user = user;
-                next();
+                const user = await User.findById(decode.id);
+                if (user) {
+                    req.user = user;
+                    next();
+                } else {
+                    throw new Error("User not found");
+                }
+            } else {
+                throw new Error("Invalid token format");
             }
-        } catch (error) {
-            throw new Error("No authorized token, Please login again")
+        } else {
+            throw new Error("Authorization header missing or invalid");
         }
-    }
-    else
-    {
-        throw new Error("There is no token with header");
+    } catch (error) {
+        res.status(401).json({ error: error.message });
     }
 });
+
 
 const isAdmin = asyncHandler(async(req,res,next)=>{
     const {email} = req.user;
--- model/orderModel.js
+++ model/orderModel.js
@@ -87,6 +87,10 @@ var orderSchema = new mongoose.Schema({
         type:Date,
         default: Date.now()
     },
+    month : {
+        type: String,
+        default: new Date().getMonth()
+    },
     totalPrice: {
         type: Number,
         required: true
