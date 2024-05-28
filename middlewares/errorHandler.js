c7e47b455242eb6e987e6669f803c8f97ebfeaea
rajon-git <rajon.zhsust15@gmail.com>
2024-05-28 22:44:05 +0600

    also some error fixed in errorhandler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,20 +1,19 @@
-// not Found
-const notFound = (res,req,next)=>{
-    const error = new Error(`Not Found: ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-}
+// notFound middleware
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found: ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
 
-// Error Handler
-
+// Error Handler middleware
 const errorHandler = (err, req, res, next) => {
-    const statuscode = res.statusCode == 200 ? 500 : res.statusCode;
-    res.status(statuscode);
-    res.json({
-      status: "fail",
-      message: err?.message,
-      stack: err?.stack,
-    });
-  };
-  
-  module.exports = { errorHandler, notFound };
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    status: "fail",
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+module.exports = { errorHandler, notFound };
